8f65013a98b2235819ee604caa4ccddcc4c06a9e
dominik-pichler <info@dominik-pichler.com>
2024-02-19 10:45:15 +0100

    WIP

--- app.py
+++ app.py
@@ -10,45 +10,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     test_me()
-    '''
-    G = nx.Graph()
-
-    ## Add nodes to the graph
-    G.add_node(
-        "test")
-    
-
-## Add edges to the graph
-    G.add_edge(
-        str("node_1"),
-        str("node_2"),
-        title="edge",
-    weight=1)
-
-    G.add_edge(
-        str("node_1"),
-        str("node_2"),
-        title="edge",
-    weight=5)
-
-    graph_output_directory = "./templates/index.html"
-
-    net = Network(
-        notebook=False,
-        # bgcolor="#1a1a1a",
-        cdn_resources="remote",
-        height="900px",
-        width="100%",
-        select_menu=True,
-        # font_color="#cccccc",
-        filter_menu=False,
-    )
-
-    net.from_nx(G)
-    net.force_atlas_2based(central_gravity=0.015, gravity=-31)
-    net.show_buttons(filter_=["physics"])
-    net.show(graph_output_directory) #notebook=False)
-'''
     # Render the Pyvis graph to HTML
     #html = net.get_html()
 

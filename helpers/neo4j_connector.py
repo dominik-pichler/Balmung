1b7dcc802b5aacbe99d1ec3931cb9693393236d5
dominik-pichler <info@dominik-pichler.com>
2024-02-23 17:11:58 +0100

    Test§

--- helpers/neo4j_connector.py
+++ helpers/neo4j_connector.py
@@ -1,22 +1,22 @@
 from py2neo import Graph, Node, Relationship
 import pandas as pd
 
+graph = Graph("bolt://localhost:7687", auth=("neo4j", "12345678"))
 
-graph = Graph("bolt://localhost:7687", auth=("neo4j", "your_password"))
+def store_df_to_neo4j(df:DataFrame) -> Exception: 
+    try:
+        for index, row in df.iterrows():
+            node1 = Node("Node", name=row['node3'])  # Assuming your node label is "Node"
+            node2 = Node("Node", name=row['node4'])
+            relationship_type = row['relationship']
+            relationship = Relationship(node1, relationship_type, node2)
+            graph.create(node1 | node2 | relationship)
+            return None
+
+    except:
+        return Exception
 
 
-# Assuming your DataFrame is named df
-df = pd.DataFrame({
-    'node1': [1, 2, 3],
-    'node2': [4, 5, 6],
-    'relationship': ['KNOWS', 'LIKES', 'FOLLOWS']
-})
 
-
-for index, row in df.iterrows():
-    node1 = Node("Node", name=row['node1'])  # Assuming your node label is "Node"
-    node2 = Node("Node", name=row['node2'])
-    relationship_type = row['relationship']
-    relationship = Relationship(node1, relationship_type, node2)
-    graph.create(node1 | node2 | relationship)
-
+if __name__ == '__main__':
+    
